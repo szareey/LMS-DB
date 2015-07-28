727485b9f65d6f119114498eda5788574d56a4d6
Sana <szareey@gmail.com>
2015-07-28 16:01:25 -0400

    start editing lessons_controller

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -7,4 +7,16 @@ class LessonsController < ApplicationController
     @course = MinistryCourse.first
   end
 
+  def create
+   # Lesson.create(
+   #    user_id: params[:user_id],
+   #    question_id: params[:question_id],
+   #    answer: params[:solution],
+   #    final_answer_img: params[:final_answer_img],
+   #    audio: params[:audio]
+   #    )
+
+    redirect_to :back
+  end
+
 end
