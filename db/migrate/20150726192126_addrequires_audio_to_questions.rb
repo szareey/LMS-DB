d2b096771d3e5cbac2a36f2c38d2792b3a57b23a
Sana <szareey@gmail.com>
2015-07-30 16:08:07 -0400

    adding custom gemfile

--- db/migrate/20150726192126_addrequires_audio_to_questions.rb
+++ db/migrate/20150726192126_addrequires_audio_to_questions.rb
@@ -1,6 +1,6 @@
 class AddrequiresAudioToQuestions < ActiveRecord::Migration
   def change
-    add_column :questions, :answer_requires_audio, :boolean, default: false
-    add_column :questions, :question_requires_audio, :boolean, default: false
+    add_column :questions, :answer_has_audio, :boolean, default: false
+    add_column :questions, :question_has_audio, :boolean, default: false
   end
 end
