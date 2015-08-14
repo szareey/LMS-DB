8b29eb64e17b2853715f3c05af99ba3f3fbfcc1d
Sergey Popov <quolpr@gmail.com>
2015-08-14 18:04:15 +0300

    Added annotation to models

--- test/models/answer_test.rb
+++ test/models/answer_test.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: answers
+#
+#  id                            :integer          not null, primary key
+#  user_id                       :integer
+#  question_id                   :integer
+#  answer                        :string(255)
+#  created_at                    :datetime
+#  updated_at                    :datetime
+#  teacher_comment               :string(255)
+#  score                         :integer
+#  request_meeting               :boolean          default(FALSE)
+#  final_answer_img_file_name    :string(255)
+#  final_answer_img_content_type :string(255)
+#  final_answer_img_file_size    :integer
+#  final_answer_img_updated_at   :datetime
+#  audio_file_name               :string(255)
+#  audio_content_type            :string(255)
+#  audio_file_size               :integer
+#  audio_updated_at              :datetime
+#
+
 require 'test_helper'
 
 class AnswerTest < ActiveSupport::TestCase
--- test/models/lesson_test.rb
+++ test/models/lesson_test.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: lessons
+#
+#  id                   :integer          not null, primary key
+#  ministry_specific_id :integer
+#  user_id              :integer
+#  lesson               :string(255)
+#  title                :string(255)
+#  difficulty           :string(255)
+#  audio_file_name      :string(255)
+#  audio_content_type   :string(255)
+#  audio_file_size      :integer
+#  audio_updated_at     :datetime
+#  homework             :string(255)
+#  created_at           :datetime
+#  updated_at           :datetime
+#
+
 require 'test_helper'
 
 class LessonTest < ActiveSupport::TestCase
