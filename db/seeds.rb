d2dcc4bc854fa13fd9c96dfb2828aa6b8d20bb5a
Sergey Popov <quolpr@gmail.com>
2015-08-20 20:21:45 +0300

    Deleted not need files + fix problem with seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
- 7gtV(9teacher = Teacher.create(
+teacher = User::Teacher.create(
     first_name: 'Sana',
     last_name: 'Zareey',
     email: 'szareey@nancycampbell.ca',
@@ -7,69 +7,69 @@
 
 Question.create(
   question: File.open("#{Rails.root}/public/questions/q1.png"),
-  user_id: teacher.id,  
+  teacher: teacher,  
   marks: 10
 )
 
 Question.create(
   question: File.open("#{Rails.root}/public/questions/q2.png"),
-  user_id: teacher.id,  
+  teacher: teacher,  
   marks: 15
 )
 
 Question.create(
   question: File.open("#{Rails.root}/public/questions/q3.png"),
-  user_id: teacher.id,  
+  teacher: teacher,  
   marks: 10
 )
 
 Question.create(
   question: File.open("#{Rails.root}/public/questions/q4.png"),
-  user_id: teacher.id,  
+  teacher: teacher,  
   marks: 15
 )
 
 Question.create(
   question: File.open("#{Rails.root}/public/questions/q5.png"),
-  user_id: teacher.id,  
+  teacher: teacher,  
   marks: 15
 )
 
 Question.create(
   question: File.open("#{Rails.root}/public/questions/q6.png"),
-  user_id: teacher.id,  
+  teacher: teacher,  
   marks: 10
 )
 
-student = Student.create(
+student = User::Student.create(
     first_name: 'Logan',
     last_name: 'Benadryl',
     email: 'logan@gmail.com',
     password: 'test123'
   )
 
-student = Student.create(
+student = User::Student.create(
     first_name: 'Jacob',
     last_name: 'Benadryl',
     email: 'jacob@gmail.com',
     password: 'test123'
   )
   
-student = Student.create(
+student = User::Student.create(
     first_name: 'Susan',
     last_name: 'Benadryl',
     email: 'susan@gmail.com',
     password: 'test123'
   )
 
-student = Student.create(
+student = User::Student.create(
     first_name: 'Gabriel',
     last_name: 'Benadryl',
     email: 'gabe@gmail.com',
     password: 'test123'
   )
 
-student = Student.create(
+student = User::Student.create(
     first_name: 'Bean',
     last_name: 'Benadryl',
     email: 'bean@gmail.com',
